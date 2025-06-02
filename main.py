4fe1fb55960b13e1a8896c850e6242b67dfdb41f
Michael Morowitz <github@worldpost.us>
2025-06-02 14:43:49 -0500

    basic application running

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import os
 import logging
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def main():
     logging.info("Starting the application...")
+    
     # load config
+    smtp_server = os.getenv('SMTP_SERVER')
+    smtp_port = os.getenv('SMTP_PORT')
+    smtp_user = os.getenv('SMTP_USER')
+    smtp_password = os.getenv('SMTP_PASSWORD')
+
     # initialize database
     # execute main logic
 
